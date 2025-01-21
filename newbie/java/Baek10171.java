79bffd457817454a14bb2619ba9d5f965812eae5
jinhyeon <jingoo211@gmail.com>
2025-01-21 23:54:18 +0900

    newbie_output

---
+++ newbie/java/Baek10171.java
@@ -0,0 +1,25 @@
+/*
+아래 예제와 같이 고양이를 출력하시오.
+\    /\
+ )  ( ')
+(  /  )
+ \(__)|
+
+ public class Main
+ */
+
+public class Baek10171 {
+    public static void main(String[] args) {
+        System.out.println(
+                "\\    /\\ \n" +
+                " )  ( ') \n" +
+                "(  /  ) \n" +
+                " \\(__)|");
+    }
+}
+
+/*
+
+"\ 를 쓰고 싶을떈 \\
+
+ */
